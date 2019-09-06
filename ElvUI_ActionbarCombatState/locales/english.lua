af2785f5c96a7b43dce1ce76322bd4e13aed8389
Darth Predator <darthpred@gmail.com>
2019-09-06 11:44:29 +0300

    New locale system

--- ElvUI_ActionbarCombatState/locales/english.lua
+++ ElvUI_ActionbarCombatState/locales/english.lua
@@ -1,6 +1,5 @@
 ﻿-- English localization file forenGB.
-local AceLocale = LibStub:GetLibrary("AceLocale-3.0");
-local L = AceLocale:NewLocale("ElvUI", "enUS", true);
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "enUS", true, true)
 if not L then return; end
 
 L['Combat State Options'] = true
